8a610b4e88d2a05358038094c0d3437dc8a94208
Ramesh Reddy <rareddy@jboss.org>
2019-08-15 11:19:20 -0500

    TEIIDSB-45: preparing the metadata for OperatorHub

--- pkg/apis/vdb/v1alpha1/doc.go
+++ pkg/apis/vdb/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the vdb v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=core.teiid.io
+// +groupName=teiid.io
 package v1alpha1
