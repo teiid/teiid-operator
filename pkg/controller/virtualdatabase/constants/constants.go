d7a85f21b1ddab621b47e4e64d08d7565111a1b8
Ramesh Reddy <rareddy@jboss.org>
2019-10-08 16:49:56 -0500

    ENTESB-11937: Adding YAML based configuration file for all versions, plus added a capability to define custom maven repositories through CRD

--- pkg/controller/virtualdatabase/constants/constants.go
+++ pkg/controller/virtualdatabase/constants/constants.go
@@ -1,57 +1,35 @@
 package constants
 
 import "github.com/teiid/teiid-operator/pkg/apis/vdb/v1alpha1"
+import "github.com/teiid/teiid-operator/pkg/util/conf"
 
 const (
 	// Version --
-	Version = "0.0.1-SNAPSHOT"
-
-	// TeiidSpringBootVersion --
-	TeiidSpringBootVersion = "1.2.0-SNAPSHOT"
-
-	// SpringBootVersion --
-	SpringBootVersion = "2.1.3.RELEASE"
-
-	// PostgreSQLVersion --
-	PostgreSQLVersion = "42.1.4"
-	// MySQLVersion --
-	MySQLVersion = "5.1.40"
-	// MongoDBVersion --
-	MongoDBVersion = "3.6.3"
-
-	// ImageStreamNamespace default namespace for the ImageStreams
-	ImageStreamNamespace = "openshift"
-	// ImageRegistry ...
-	ImageRegistry = "docker.io"
-	// ImageRepo ...
-	ImageRepo = "fabric8"
-	// ImageStreamTag default tag name for the ImageStreams
-	ImageStreamTag = "latest" // "latest-java11"
+	Version = "0.0.1"
 	// BuilderImageTargetName target build image name
 	BuilderImageTargetName = "virtualdatabase-builder"
-	// S2IBuildImageName --
-	S2IBuildImageName = "s2i-java"
-	//S2IBuildImageTag --
-	S2IBuildImageTag = "latest-java11"
 	// SpringBoot --
 	SpringBoot = "SpringBoot"
 )
 
+// Config from /conf/config.yml file
+var Config = conf.GetConfiguration()
+
 // SpringBootRuntime --
 var SpringBootRuntime = v1alpha1.RuntimeType{
 	Type:    SpringBoot,
-	Version: TeiidSpringBootVersion,
+	Version: Config.TeiidSpringBootVersion,
 }
 
 // RuntimeImageDefaults ...
 var RuntimeImageDefaults = map[string][]v1alpha1.Image{
 	SpringBootRuntime.Type: {
 		{
-			ImageStreamName:      S2IBuildImageName,
-			ImageStreamNamespace: ImageStreamNamespace,
-			ImageStreamTag:       S2IBuildImageTag,
-			ImageRegistry:        ImageRegistry,
-			ImageRepository:      ImageRepo,
+			ImageStreamName:      Config.BuildImage.Name,
+			ImageStreamNamespace: Config.BuildImage.Namespace,
+			ImageStreamTag:       Config.BuildImage.Tag,
+			ImageRegistry:        Config.BuildImage.Registry,
+			ImageRepository:      Config.BuildImage.Repo,
 			BuilderImage:         true,
 		},
 		{
---
+++ pkg/util/conf/conf.go
@@ -0,0 +1,61 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package conf
+
+import (
+	"io/ioutil"
+
+	"github.com/teiid/teiid-operator/pkg/util/logs"
+	"gopkg.in/yaml.v2"
+)
+
+// Configuration --
+type Configuration struct {
+	TeiidSpringBootVersion string            `yaml:"teiidSpringBootVersion,omitempty"`
+	SpringBootVersion      string            `yaml:"springBootVersion,omitempty"`
+	MavenRepositories      map[string]string `yaml:"mavenRepositories,omitempty"`
+	Productized            bool              `yaml:"productized,omitempty"`
+	EarlyAccess            bool              `yaml:"earlyAccess,omitempty"`
+	BuildImage             BuildImage        `yaml:"buildImage,omitempty"`
+	Drivers                map[string]string `yaml:"drivers,omitempty"`
+}
+
+// BuildImage --
+type BuildImage struct {
+	Registry  string `yaml:"registry,omitempty"`
+	Repo      string `yaml:"repo,omitempty"`
+	Name      string `yaml:"name,omitempty"`
+	Tag       string `yaml:"tag,omitempty"`
+	Namespace string `yaml:"namespace,omitempty"`
+}
+
+// GetConfiguration --
+func GetConfiguration() Configuration {
+	log := logs.GetLogger("configuration")
+
+	var c Configuration
+	yamlFile, err := ioutil.ReadFile("/conf/config.yaml")
+	if err != nil {
+		log.Error("Failed to read configuration file ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		log.Error("Unmarshal: %v", err)
+	}
+	log.Info("Configuration:", c)
+	return c
+}
