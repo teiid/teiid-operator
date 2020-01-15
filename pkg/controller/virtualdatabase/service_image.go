29d8eefadea2dc892b44f476fda70b451526bc0e
Ramesh Reddy <rareddy@jboss.org>
2020-01-14 19:31:11 -0600

    TEIIDTOOLS-889: adding support for FAT JAR based Virtual Database deployment where the FAT JAR is located in a maven repository (#52)

--- pkg/controller/virtualdatabase/service_image.go
+++ pkg/controller/virtualdatabase/service_image.go
@@ -79,17 +79,20 @@ func (action *serviceImageAction) buildServiceImage(ctx context.Context, vdb *v1
 	}
 
 	// Define new BuildConfig objects
-	buildConfig := action.newServiceBC(vdb)
-	if _, err := image.EnsureImageStream(buildConfig.Name, vdb.ObjectMeta.Namespace, true, vdb, r.imageClient, r.scheme); err != nil {
+	if _, err := image.EnsureImageStream(vdb.ObjectMeta.Name, vdb.ObjectMeta.Namespace, true, vdb, r.imageClient, r.scheme); err != nil {
 		return err
 	}
 
 	// Check if this BC already exists
-	bc, err := r.buildClient.BuildConfigs(buildConfig.Namespace).Get(buildConfig.Name, metav1.GetOptions{})
+	bc, err := r.buildClient.BuildConfigs(vdb.ObjectMeta.Namespace).Get(vdb.ObjectMeta.Name, metav1.GetOptions{})
 	if err != nil && apierr.IsNotFound(err) {
-		log.Info("Creating a new BuildConfig ", buildConfig.Name, " in namespace ", buildConfig.Namespace)
+		log.Info("Creating a new BuildConfig ", vdb.ObjectMeta.Name, " in namespace ", vdb.ObjectMeta.Namespace)
 		// set ownerreference for service BC only
-		err := controllerutil.SetControllerReference(vdb, &buildConfig, r.scheme)
+		buildConfig, err := action.newServiceBC(vdb)
+		if err != nil {
+			return err
+		}
+		err = controllerutil.SetControllerReference(vdb, &buildConfig, r.scheme)
 		if err != nil {
 			log.Error(err)
 		}
@@ -107,15 +110,40 @@ func (action *serviceImageAction) buildServiceImage(ctx context.Context, vdb *v1
 	// Trigger first build of "builder" and binary BCs
 	if bc.Status.LastVersion == 0 || digest.Value != vdb.Status.Digest {
 		envvar.SetVal(&bc.Spec.Strategy.SourceStrategy.Env, "DIGEST", vdb.Status.Digest)
+
 		if err := r.client.Update(ctx, bc); err != nil {
 			return err
 		}
 
-		if err = action.triggerBuild(*bc, vdb, r); err != nil {
+		var payload map[string]string
+		if isDockerBuild(vdb) {
+			files, err := buildJarBasedPayload(vdb)
+			if err != nil {
+				return nil
+			}
+			payload = files
+		} else {
+			files, err := buildVdbBasedPayload(vdb)
+			if err != nil {
+				return nil
+			}
+			payload = files
+		}
+
+		if err = action.triggerBuild(*bc, payload, r); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func isDockerBuild(vdb *v1alpha1.VirtualDatabase) bool {
+	if vdb.Spec.Build.Source.Maven != "" {
+		if !strings.Contains(vdb.Spec.Build.Source.Maven, ":vdb:") {
+			return true
+		}
+	}
+	return false
 }
 
 func (action *serviceImageAction) monitorServiceImage(ctx context.Context, vdb *v1alpha1.VirtualDatabase, r *ReconcileVirtualDatabase) error {
@@ -157,8 +185,8 @@ func (action *serviceImageAction) monitorServiceImage(ctx context.Context, vdb *
 	return nil
 }
 
-func (action *serviceImageAction) newServiceBC(vdb *v1alpha1.VirtualDatabase) obuildv1.BuildConfig {
-	baseImage := corev1.ObjectReference{Name: strings.Join([]string{constants.BuilderImageTargetName, "latest"}, ":"), Kind: "ImageStreamTag"}
+func (action *serviceImageAction) newServiceBC(vdb *v1alpha1.VirtualDatabase) (obuildv1.BuildConfig, error) {
+	baseImage := strings.Join([]string{constants.BuilderImageTargetName, "latest"}, ":")
 
 	// set it back original default
 	envvar.SetVal(&vdb.Spec.Build.Env, "DEPLOYMENTS_DIR", "/deployments")
@@ -179,31 +207,153 @@ func (action *serviceImageAction) newServiceBC(vdb *v1alpha1.VirtualDatabase) ob
 	bc.SetGroupVersionKind(obuildv1.SchemeGroupVersion.WithKind("BuildConfig"))
 	bc.Spec.Output.To = &corev1.ObjectReference{Name: strings.Join([]string{vdb.ObjectMeta.Name, "latest"}, ":"), Kind: "ImageStreamTag"}
 
-	// for some reason "vdb.Spec.Build.GitSource" comes in as empty object rather than nil
-	if vdb.Spec.Build.Source.DDL != "" {
-		log.Info("DDL based build is chosen..")
-		bc.Spec.Source.Binary = &obuildv1.BinaryBuildSource{}
-	} else if vdb.Spec.Build.Source.Maven != "" {
-		log.Info("Maven based VDB build is chosen..")
-		bc.Spec.Source.Binary = &obuildv1.BinaryBuildSource{}
-	}
-
+	// for some reason "vdb.Spec.Build.Source" comes in as empty object rather than nil
+	// create the source build object
+	bc.Spec.Source.Type = obuildv1.BuildSourceBinary
+	bc.Spec.Source.Binary = &obuildv1.BinaryBuildSource{}
 	bc.Spec.Strategy.Type = obuildv1.SourceBuildStrategyType
 	bc.Spec.Strategy.SourceStrategy = &obuildv1.SourceBuildStrategy{
-		From:        baseImage,
+		From:        corev1.ObjectReference{Name: baseImage, Kind: "ImageStreamTag"},
 		ForcePull:   false,
 		Incremental: vdb.Spec.Build.Incremental,
 		Env:         vdb.Spec.Build.Env,
 	}
-	bc.Spec.Source.Type = obuildv1.BuildSourceImage
+
+	if vdb.Spec.Build.Source.DDL != "" {
+		log.Info("DDL based build is chosen..")
+	} else if vdb.Spec.Build.Source.Maven != "" {
+		if strings.Contains(vdb.Spec.Build.Source.Maven, ":vdb:") {
+			log.Info("Maven based VDB build is chosen..")
+		} else {
+			log.Info("Maven Repo Fat Jar based Docker build is chosen..")
+		}
+	}
+
 	// when trigger is defined the build starts immediately without the
 	// binary, using previous base build's source directory which is not
 	// intended result, so do not add triggers
-	return bc
+	return bc, nil
+}
+
+func readDdlFromMavenRepo(vdb *v1alpha1.VirtualDatabase, targetName string) (string, error) {
+	dep, err := maven.ParseGAV(vdb.Spec.Build.Source.Maven)
+	if err != nil {
+		return "", err
+	}
+	vdbFile, err := maven.DownloadDependency(dep, targetName, vdb.Spec.Build.Source.MavenRepositories)
+	if err != nil {
+		return "", err
+	}
+	files, err := zip.Unzip(vdbFile, "/tmp/"+vdb.ObjectMeta.Name)
+	if err != nil {
+		return "", err
+	}
+	log.Info("Maven based VDB file contains files: ", files)
+	b, err := ioutil.ReadFile("/tmp/" + vdb.ObjectMeta.Name + "/META-INF/vdb.ddl")
+	if err != nil {
+		return "", err
+	}
+	ddl := string(b)
+	log.Debug("Read VDB File: " + ddl)
+	return ddl, nil
+}
+
+func buildJarBasedPayload(vdb *v1alpha1.VirtualDatabase) (map[string]string, error) {
+	files := map[string]string{}
+
+	//Binary build, generate the pom file
+	pom, err := GenerateJarPom(vdb)
+	if err != nil {
+		return files, err
+	}
+
+	jarDependency, err := maven.ParseGAV(vdb.Spec.Build.Source.Maven)
+	if err != nil {
+		log.Error("The Maven based JAR is provided in bad format", err)
+		return files, err
+	}
+	// configure pom.xml to copy the teiid.vdb into classpath
+	addCopyPlugIn(jarDependency, "jar", "app.jar", "${project.build.directory}", &pom)
+
+	pomContent, err := maven.GeneratePomContent(pom)
+	if err != nil {
+		return files, err
+	}
+
+	log.Info("Pom file generated %s", pomContent)
+
+	files["/pom.xml"] = pomContent
+	files["/src/main/resources/prometheus-config.yml"] = PrometheusConfig()
+
+	return files, nil
+}
+
+func buildVdbBasedPayload(vdb *v1alpha1.VirtualDatabase) (map[string]string, error) {
+	files := map[string]string{}
+
+	// add OpenAPI document
+	addOpenAPI := false
+	if len(vdb.Spec.Build.Source.OpenAPI) > 0 {
+		files["/src/main/resources/openapi.json"] = vdb.Spec.Build.Source.OpenAPI
+		addOpenAPI = true
+	}
+
+	// check to make sure which type of vdb
+	mavenVdb := false
+	var ddl string
+	ddl = vdb.Spec.Build.Source.DDL
+	if vdb.Spec.Build.Source.Maven != "" {
+		mavenVdb = true
+		str, err := readDdlFromMavenRepo(vdb, "/tmp/teiid.vdb")
+		if err != nil {
+			log.Error("failed to read VDB from maven ", err)
+			return files, err
+		}
+		ddl = str
+	}
+
+	//Binary build, generate the pom file
+	pom, err := GenerateVdbPom(vdb, ddl, false, addOpenAPI)
+	if err != nil {
+		return files, err
+	}
+
+	if mavenVdb {
+		// vdb-code-gen plugin is finding the vdb.ddl file before the dependency
+		// or the .vdb file from the base image, this is way hard code to use the
+		// correct vdb file.
+		addVdbCodeGenPlugIn(&pom, "/tmp/teiid.vdb")
+		// maven based vdb is given
+		vdbDependency, err := maven.ParseGAV(vdb.Spec.Build.Source.Maven)
+		if err != nil {
+			log.Error("The Maven based VDB is provided in bad format", err)
+			return files, err
+		}
+		// Add VDB as dependency
+		pom.AddDependencies(vdbDependency)
+		// configure pom.xml to copy the teiid.vdb into classpath
+		addCopyPlugIn(vdbDependency, "vdb", "teiid.vdb", "${project.build.outputDirectory}", &pom)
+	} else {
+		addVdbCodeGenPlugIn(&pom, "/tmp/src/src/main/resources/teiid.ddl")
+		files["/src/main/resources/teiid.ddl"] = vdb.Spec.Build.Source.DDL
+	}
+
+	pomContent, err := maven.GeneratePomContent(pom)
+	if err != nil {
+		return files, err
+	}
+
+	log.Debugf("Pom file generated %s", pomContent)
+
+	files["/pom.xml"] = pomContent
+	files["/src/main/resources/prometheus-config.yml"] = PrometheusConfig()
+	files["/src/main/resources/application.properties"] = applicationProperties(mavenVdb)
+
+	return files, nil
 }
 
 // triggerBuild triggers a BuildConfig to start a new build
-func (action *serviceImageAction) triggerBuild(bc obuildv1.BuildConfig, vdb *v1alpha1.VirtualDatabase, r *ReconcileVirtualDatabase) error {
+func (action *serviceImageAction) triggerBuild(bc obuildv1.BuildConfig, files map[string]string, r *ReconcileVirtualDatabase) error {
 	log := log.With("kind", "BuildConfig", "name", bc.GetName(), "namespace", bc.GetNamespace())
 	buildConfig, err := r.buildClient.BuildConfigs(bc.Namespace).Get(bc.Name, metav1.GetOptions{})
 	if err != nil {
@@ -211,76 +361,6 @@ func (action *serviceImageAction) triggerBuild(bc obuildv1.BuildConfig, vdb *v1a
 	}
 	if buildConfig.Spec.Source.Type == obuildv1.BuildSourceBinary {
 		log.Info("starting the binary build for service image ")
-		files := map[string]string{}
-
-		// add OpenAPI document
-		addOpenAPI := false
-		if len(vdb.Spec.Build.Source.OpenAPI) > 0 {
-			files["/src/main/resources/openapi.json"] = vdb.Spec.Build.Source.OpenAPI
-			addOpenAPI = true
-		}
-
-		// check to make sure which type of vdb
-		mavenVdb := false
-		ddl := vdb.Spec.Build.Source.DDL
-		if vdb.Spec.Build.Source.DDL != "" {
-			mavenVdb = false
-		} else if vdb.Spec.Build.Source.Maven != "" {
-			mavenVdb = true
-			vdbFile, err := readVdbFromMaven(vdb, "/tmp/teiid.vdb")
-			if err != nil {
-				return err
-			}
-			files, err := zip.Unzip(vdbFile, "/tmp/"+vdb.ObjectMeta.Name)
-			if err != nil {
-				return err
-			}
-			log.Info("Maven based VDB file contains files: ", files)
-			b, err := ioutil.ReadFile("/tmp/" + vdb.ObjectMeta.Name + "/META-INF/vdb.ddl")
-			if err != nil {
-				return err
-			}
-			ddl = string(b)
-			log.Debug("Read VDB File: " + ddl)
-		}
-
-		//Binary build, generate the pom file
-		pom, err := GeneratePom(vdb, ddl, false, addOpenAPI)
-		if err != nil {
-			return nil
-		}
-
-		if mavenVdb {
-			// vdb-code-gen plugin is finding the vdb.ddl file before the dependency
-			// or the .vdb file from the base image, this is way hard code to use the
-			// correct vdb file.
-			addVdbCodeGenPlugIn(&pom, "/tmp/teiid.vdb")
-			// maven based vdb is given
-			vdbDependency, err := maven.ParseGAV(vdb.Spec.Build.Source.Maven)
-			if err != nil {
-				log.Error("The Maven based VDB is provided in bad format", err)
-				return err
-			}
-			// Add VDB as dependency
-			pom.AddDependencies(vdbDependency)
-			// configure pom.xml to copy the teiid.vdb into classpath
-			addCopyPlugIn(vdbDependency, &pom)
-		} else {
-			addVdbCodeGenPlugIn(&pom, "/tmp/src/src/main/resources/teiid.ddl")
-			files["/src/main/resources/teiid.ddl"] = vdb.Spec.Build.Source.DDL
-		}
-
-		pomContent, err := maven.GeneratePomContent(pom)
-		if err != nil {
-			return err
-		}
-
-		log.Debugf("Pom file generated %s", pomContent)
-
-		files["/pom.xml"] = pomContent
-		files["/src/main/resources/prometheus-config.yml"] = PrometheusConfig()
-		files["/src/main/resources/application.properties"] = action.applicationProperties(mavenVdb)
-
 		tarReader, err := util.Tar(files)
 		if err != nil {
 			return err
@@ -296,7 +376,7 @@ func (action *serviceImageAction) triggerBuild(bc obuildv1.BuildConfig, vdb *v1a
 			binaryBuildRequest.SetGroupVersionKind(obuildv1.SchemeGroupVersion.WithKind("BinaryBuildRequestOptions"))
 			log.Info("Triggering binary build ", buildConfig.Name)
 			err = r.buildClient.RESTClient().Post().
-				Namespace(vdb.ObjectMeta.Namespace).
+				Namespace(bc.ObjectMeta.Namespace).
 				Resource("buildconfigs").
 				Name(buildConfig.Name).
 				SubResource("instantiatebinary").
@@ -311,7 +391,7 @@ func (action *serviceImageAction) triggerBuild(bc obuildv1.BuildConfig, vdb *v1a
 	} else {
 		buildRequest := obuildv1.BuildRequest{ObjectMeta: metav1.ObjectMeta{Name: buildConfig.Name}}
 		buildRequest.SetGroupVersionKind(obuildv1.SchemeGroupVersion.WithKind("BuildRequest"))
-		buildRequest.TriggeredBy = []obuildv1.BuildTriggerCause{{Message: fmt.Sprintf("Triggered by %s operator", vdb.Kind)}}
+		buildRequest.TriggeredBy = []obuildv1.BuildTriggerCause{{Message: fmt.Sprintf("Triggered by %s operator", "VirtualDatabase")}}
 		log.Info("Triggering build ", buildConfig.Name)
 		_, err := r.buildClient.BuildConfigs(buildConfig.Namespace).Instantiate(buildConfig.Name, &buildRequest)
 		if err != nil {
@@ -321,7 +401,7 @@ func (action *serviceImageAction) triggerBuild(bc obuildv1.BuildConfig, vdb *v1a
 	return nil
 }
 
-func (action *serviceImageAction) applicationProperties(addVDB bool) string {
+func applicationProperties(addVDB bool) string {
 	str := `logging.level.io.jaegertracing.internal.reporters=WARN
 	logging.level.i.j.internal.reporters.LoggingReporter=WARN
 	logging.level.org.teiid.SECURITY=WARN
@@ -337,12 +417,4 @@ func (action *serviceImageAction) applicationProperties(addVDB bool) string {
 		str = str + "teiid.vdb-file=teiid.vdb\n"
 	}
 	return str
-}
-
-func readVdbFromMaven(vdb *v1alpha1.VirtualDatabase, targetName string) (string, error) {
-	dep, err := maven.ParseGAV(vdb.Spec.Build.Source.Maven)
-	if err != nil {
-		return "", err
-	}
-	return maven.DownloadDependency(dep, targetName, vdb.Spec.Build.Source.MavenRepositories)
 }
