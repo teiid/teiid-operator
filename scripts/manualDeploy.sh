81946b37f82d41b49824aae1ad4159b400bbb2fd
Steven Hawkins <shawkins@redhat.com>
2020-04-02 14:22:20 -0400

    ENTESB-13287 a minor correction and defaulting to latest

--- scripts/manualDeploy.sh
+++ scripts/manualDeploy.sh
@@ -9,7 +9,7 @@ then
   IMAGE=$CRC_IMAGE  
 fi
 
-sed "s|quay\.io\/teiid\/teiid-operator\:0\.3\.0|${IMAGE}|g" deploy/operator.yaml > deploy/operator-`whoami`.yaml
+sed "s|quay\.io\/teiid\/teiid-operator\:latest|${IMAGE}|g" deploy/operator.yaml > deploy/operator-`whoami`.yaml
 
 for FILE in deploy/crds/teiid.io_virtualdatabases_crd.yaml deploy/role.yaml deploy/service_account.yaml deploy/role_binding.yaml deploy/operator-`whoami`.yaml
 do
