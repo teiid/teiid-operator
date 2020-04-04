4e93f610b35eea53d385390b3e8f16c8e122aaff
Ramesh Reddy <rareddy@jboss.org>
2020-04-04 12:53:27 -0500

    TEIIDTOOLS-975: Handling the Camel Case names more gracefully in Datasource names

--- pkg/controller/virtualdatabase/convert-ds-properties_test.go
+++ pkg/controller/virtualdatabase/convert-ds-properties_test.go
@@ -71,8 +71,9 @@ func TestSpringProperties(t *testing.T) {
 		},
 	}
 
-	envs := convert2SpringProperties(datasources)
+	envs, err := convert2SpringProperties(datasources)
 	assert.NotNil(t, envs)
+	assert.Nil(t, err)
 
 	expected := []corev1.EnvVar{
 		{
@@ -80,8 +81,108 @@ func TestSpringProperties(t *testing.T) {
 			Value: "localhost:11222",
 		},
 		{
-			Name:  "SPRING_TEIID_DATA_INFINISPAN_DG_IMPORTER_PROTOBUFNAME",
+			Name:  "SPRING_TEIID_DATA_INFINISPAN_DG_IMPORTER_PROTOBUF_NAME",
 			Value: "accounts.proto",
+		},
+		{
+			Name:  "SPRING_DATASOURCE_SAMPLEDB_JDBC_URL",
+			Value: "jdbc:postgresql://localhost:5432/sampledb",
+		},
+		{
+			Name:      "SPRING_DATASOURCE_SAMPLEDB_PASSWORD",
+			ValueFrom: &source,
+		},
+	}
+	assert.Equal(t, expected, envs)
+}
+
+func TestUpperCase(t *testing.T) {
+	assert.Equal(t, []string{"foo", "Bar"}, tokenizeAtUpperCase("fooBar"))
+	assert.Equal(t, []string{"foo", "B", "A", "R"}, tokenizeAtUpperCase("fooBAR"))
+	assert.Equal(t, []string{"Foo", "B", "A", "R"}, tokenizeAtUpperCase("FooBAR"))
+}
+
+func TestSanitizeName(t *testing.T) {
+	assert.Equal(t, "foo-bar", sanitizeName("fooBar"))
+	assert.Equal(t, "foo-bar", sanitizeName("fooBAR"))
+	assert.Equal(t, "foo-bar", sanitizeName("FooBAR"))
+	assert.Equal(t, "foo-bar", sanitizeName("foo-bar"))
+	assert.Equal(t, "foobar", sanitizeName("foobar"))
+	assert.Equal(t, "foo.bar", sanitizeName("foo.bar"))
+	assert.Equal(t, "foo.bar", sanitizeName("foo.Bar"))
+	assert.Equal(t, "foo-bar", sanitizeName("foo-Bar"))
+	assert.Equal(t, "foo-bar", sanitizeName("Foo-Bar"))
+}
+
+func TestSoapProperties(t *testing.T) {
+	source := corev1.EnvVarSource{
+		ConfigMapKeyRef: &corev1.ConfigMapKeySelector{
+			Key: "foo",
+			LocalObjectReference: corev1.LocalObjectReference{
+				Name: "map-name",
+			},
+		},
+	}
+
+	datasources := []v1alpha1.DataSourceObject{
+		{
+			Name: "soapCountry",
+			Type: "soap",
+			Properties: []corev1.EnvVar{
+				{
+					Name:  "wsdl",
+					Value: "http://www.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL",
+				},
+				{
+					Name:  "namespaceUri",
+					Value: "http://www.oorsprong.org/websamples.countryinfo",
+				},
+				{
+					Name:  "serviceName",
+					Value: "CountryInfoService",
+				},
+				{
+					Name:  "endPointName",
+					Value: "CountryInfoServiceSoap12",
+				},
+			},
+		},
+		{
+			Name: "sampledb",
+			Type: "postgresql",
+			Properties: []corev1.EnvVar{
+				{
+					Name:  "jdbc-url",
+					Value: "jdbc:postgresql://localhost:5432/sampledb",
+				},
+				{
+					Name:      "password",
+					ValueFrom: &source,
+				},
+			},
+		},
+	}
+
+	envs, err := convert2SpringProperties(datasources)
+	assert.NotNil(t, envs)
+	assert.Nil(t, err)
+
+	expected := []corev1.EnvVar{
+		{
+			Name:  "SPRING_TEIID_DATA_SOAP_SOAP_COUNTRY_WSDL",
+			Value: "http://www.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL",
+		},
+		{
+			Name:  "SPRING_TEIID_DATA_SOAP_SOAP_COUNTRY_NAMESPACE_URI",
+			Value: "http://www.oorsprong.org/websamples.countryinfo",
+		},
+		{
+			Name:  "SPRING_TEIID_DATA_SOAP_SOAP_COUNTRY_SERVICE_NAME",
+			Value: "CountryInfoService",
+		},
+		{
+			Name:  "SPRING_TEIID_DATA_SOAP_SOAP_COUNTRY_END_POINT_NAME",
+			Value: "CountryInfoServiceSoap12",
 		},
 		{
 			Name:  "SPRING_DATASOURCE_SAMPLEDB_JDBC_URL",
