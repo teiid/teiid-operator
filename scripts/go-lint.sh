e3adc42a9fd7c27bc914934312ef7231a41b95c5
Steven Hawkins <shawkins@redhat.com>
2020-08-04 17:39:42 -0400

    TEIIDSB-223 switching to make ci (#187)

--- scripts/go-lint.sh
+++ scripts/go-lint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 yamllint --no-warnings -c scripts/yamllint.config deploy/crs
-dirs=(cmd pkg version)
+dirs=(cmd pkg)
 for dir in "${dirs[@]}"
 do
     if ! golint -set_exit_status ${dir}/...; then
